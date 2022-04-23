1003953a36c265eff67dd42298689c721dc368d7
937249216 <57359304+937249216@users.noreply.github.com>
2022-04-23 23:54:03 +0800

    12

--- pycharm/io.py
+++ pycharm/io.py
@@ -1,13 +1,25 @@
-import os
+import turtle
+import random
+
+turtle.setup(1280.720)
+turtle.bgpic("1233.png")
+turtle.speed(1)
+turtle.pencolor('white')
+turtle.fillcolor('white')
 
 
-def getAllFiles(path, level):
-    childfiles = os.listdir(path)
-    for file in childfiles:
-        filepath = os.path.join(path, file)
-        if os.path.isdir(filepath):
-            getAllFiles(filepath, filepath)
-        print("\t" * level + filepath)
+def star_creat():
+    turtle.penup()
+    turtle.goto(random.randint(-600, 600)), random.randint(-300, 300)
+    turtle.pendown()
 
-
-getAllFiles("../learn", 0)
+    s=random.randint(5,11)
+    if s%2==1:
+        turtle.begin_fill()
+        for i in range(s):
+            turtle.left(180-180/s)
+            turtle.forward(50)
+        turtle.end_fill()
+for i in range(1000)
+    star_creat()
+turtle.done    
